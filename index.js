782a0a638b7d9001db9267f968d49cc8eb58a823
Melanie Rolando <melanie.nrolando@gmail.com>
2026-07-02 16:26:22 -0300

    Add Not Found Middleware

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 const app = express();
 
+app.use((req, res, next) => {
+    // res.json({message : "API en construccion" })
+    console.log(req.method);
+    next();
+})
+
 app.get('/', (req, res) => {
-    res.send('Bienvenidos a mi API REST!');
+    res.json({'message' : "Bienvenidos a mi API REST Entrega"});
 });
+
+import notFound from "./src/middlewares/not-found.js";
+app.use(notFound);
 
 const PORT = 3000;
 
